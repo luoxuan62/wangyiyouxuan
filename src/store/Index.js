7a5cb162186fe663b8ddfe2065dc91446186c6bf
huangqijina <1748514873@qq.com>
2019-07-08 09:25:05 +0800

    234

--- src/store/Index.js
+++ src/store/Index.js
@@ -12,5 +12,3 @@
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const store = createStore (reducer,composeEnhancers(applyMiddleware(thunk)));
 
-
-// export default store;
